3390e69c0428d220df30b4086a1c3a3bade1cc00
jhonatanleon01 <jhonatanleonlucio40@gmail.com>
2026-03-27 21:17:33 -0500

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -200,7 +200,7 @@ const RUTA_GREEN_FOREST = crearRutaDesdeGrid(GRID_GREEN_FOREST, {
 });
 
 const MAPAS_RUTAS = {
-    bosque: GRID_GREEN_FOREST,
+    bosque: RUTA_GREEN_FOREST,
     cueva: RUTA_CONTORNO_BASE,
     lago: RUTA_CONTORNO_BASE,
     torre: RUTA_CONTORNO_BASE,
