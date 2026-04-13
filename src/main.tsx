b1b5449abd0ebb15e11dabe5f6161bb964d0119c
jhonatanleon01 <90358970+Leonlucio01@users.noreply.github.com>
2026-04-13 03:58:41 -0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,19 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { queryClient } from "@/shared/api/queryClient";
 import { router } from "@/app/router";
-import { env } from "@/shared/config/env";
 import "@/index.css";
+
+const GOOGLE_CLIENT_ID =
+  "353230935122-vqqd262fjhetd408li95420bb8cas5vb.apps.googleusercontent.com";
+
+(window as any).__GOOGLE_CLIENT_ID__ = GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={env.googleClientId}>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-console.log("GOOGLE CLIENT ID:", env.googleClientId);
